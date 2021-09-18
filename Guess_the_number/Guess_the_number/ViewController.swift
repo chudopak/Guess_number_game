b892640909047b845a8d98f1b4174a599562d689
Stepan Kirillov <stepakirillov59@gmail.com>
2021-09-18 20:33:19 +0300

    Slider is moving

--- Guess_the_number/Guess_the_number/ViewController.swift
+++ Guess_the_number/Guess_the_number/ViewController.swift
@@ -7,21 +7,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct AlertData {
+	let controllerTitle: String
+	let controllerMessage: String
+	let actionTitle: String
+}
 
+class ViewController: UIViewController
+{
+	
+	private var _currentSliderValue: Int = 1
+	private var _targetValue: Int = 0
+	private let _failAlert		= AlertData(controllerTitle: "You failed",
+										  controllerMessage: "Sucker, target value is: ",
+										        actionTitle: "Sorry masta")
+	private let _successAlert	= AlertData(controllerTitle: "You Did well",
+										  controllerMessage: "pussy BOIIIII, target value is:",
+											    actionTitle: "Ok")
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		_targetValue = 1 + Int(arc4random_uniform(100))
 		// Do any additional setup after loading the view.
 	}
 
-	@IBAction func showAlert() {
-		let alert = UIAlertController(title: "Hello, World",
-									  message: "This is my first app!",
+	private func _showFailAlert() {
+		let controllerMsg = _failAlert.controllerMessage + String(_targetValue)
+		let alert = UIAlertController(title: _failAlert.controllerTitle,
+									  message: controllerMsg,
 									  preferredStyle: .alert)
-		let action = UIAlertAction(title: "Awesome", style: .default,
+		let action = UIAlertAction(title: _failAlert.actionTitle, style: .cancel,
 								   handler: nil)
 		alert.addAction(action)
 		present(alert, animated: true, completion: nil)
+	}
+	
+	private func _showSuccessAlert() {
+		let controllerMsg = _successAlert.controllerMessage + String(_targetValue)
+		let alert = UIAlertController(title: _successAlert.controllerTitle,
+									  message: controllerMsg,
+									  preferredStyle: .alert)
+		let action = UIAlertAction(title: _successAlert.actionTitle, style: .cancel,
+								   handler: nil)
+		alert.addAction(action)
+		present(alert, animated: true, completion: nil)
+	}
+
+	@IBAction func popUpAlert() {
+		let difference = _currentSliderValue - _targetValue
+		if (-10 < difference && difference < 10) {
+			_showSuccessAlert()
+		} else {
+			_showFailAlert()
+		}
+		_targetValue = 1 + Int(arc4random_uniform(100))
+	}
+	
+	@IBAction func sliderMoved(_ slider: UISlider) {
+		_currentSliderValue = Int(slider.value)
 	}
 }
 
