6a555a9f52b2a2f805d397fd0e361888dbdaf506
Stepan Kirillov <stepakirillov59@gmail.com>
2021-09-28 21:03:23 +0500

    added score, refreshed code

--- Guess_the_number/Guess_the_number/ViewController.swift
+++ Guess_the_number/Guess_the_number/ViewController.swift
@@ -18,39 +18,66 @@ class ViewController: UIViewController
 	
 	private var _currentSliderValue: Int = 0
 	private var _targetValue: Int = 0
-	private let _failAlert		= AlertData(controllerTitle: "You failed",
-										  controllerMessage: "Sucker, target value is: ",
-										        actionTitle: "Sorry masta")
-	private let _successAlert	= AlertData(controllerTitle: "You Did well",
-										  controllerMessage: "pussy BOIIIII, target value is:",
-											    actionTitle: "Ok")
+	private var _score: Int = 0
+	private var _round: Int = 0
+	private let _sliderMaxValue: Int = 100
+	private let _maxDifferenceFromTarget: Int = 3
+	private let _maxTarget: Int = 100
+	private let _minTarget: Int = 1
+	
 	@IBOutlet weak var slider: UISlider!
+	@IBOutlet weak var score: UILabel!
+	@IBOutlet weak var round: UILabel!
+	@IBOutlet weak var maxTargetLabel: UILabel!
+	@IBOutlet weak var minTargetLabel: UILabel!
+	@IBOutlet weak var targetLabel: UILabel!
+	
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		_targetValue = 1 + Int(arc4random_uniform(100))
-		_currentSliderValue = Int(slider.value)
-		print(_currentSliderValue)
+		_setNewRound()
+		_setStartScreen()
 		// Do any additional setup after loading the view.
 	}
-
-	private func _showFailAlert() {
-		let controllerMsg = _failAlert.controllerMessage + String(_targetValue)
-		let alert = UIAlertController(title: _failAlert.controllerTitle,
-									  message: controllerMsg,
+	
+	private func _setStartScreen() {
+		maxTargetLabel.text = String(_maxTarget)
+		minTargetLabel.text = String(_minTarget)
+		_updateLabels()
+	}
+	
+	private func _updateLabels() {
+		score.text = String(_score)
+		targetLabel.text = String(_targetValue)
+		round.text = String(_round)
+	}
+	
+	private func _setNewRound() {
+		_round += 1
+		_targetValue = 1 + Int(arc4random_uniform(UInt32(_sliderMaxValue)))
+		slider.value = Float(_sliderMaxValue / 2)
+		_currentSliderValue = Int(slider.value)
+	}
+	
+	private func _showFailAlert(difference: Int) {
+		let alert = UIAlertController(title: "Failure!",
+									  message: "The target Value is \(_targetValue)\n" +
+											   "The current Value is \(_currentSliderValue)\n" +
+											   "The difference is \(abs(difference))\n",
 									  preferredStyle: .alert)
-		let action = UIAlertAction(title: _failAlert.actionTitle, style: .cancel,
+		let action = UIAlertAction(title: "FOOOk, I will try again!", style: .cancel,
 								   handler: nil)
 		alert.addAction(action)
 		present(alert, animated: true, completion: nil)
 	}
 	
-	private func _showSuccessAlert() {
-		let controllerMsg = _successAlert.controllerMessage + String(_targetValue)
-		let alert = UIAlertController(title: _successAlert.controllerTitle,
-									  message: controllerMsg,
+	private func _showSuccessAlert(difference: Int) {
+		let alert = UIAlertController(title: "Success!",
+									  message: "The target Value is \(_targetValue)\n" +
+											   "The current Value is \(_currentSliderValue)\n" +
+											   "The difference is \(abs(difference))",
 									  preferredStyle: .alert)
-		let action = UIAlertAction(title: _successAlert.actionTitle, style: .cancel,
+		let action = UIAlertAction(title: "My Bigga", style: .cancel,
 								   handler: nil)
 		alert.addAction(action)
 		present(alert, animated: true, completion: nil)
@@ -58,12 +85,14 @@ class ViewController: UIViewController
 
 	@IBAction func popUpAlert() {
 		let difference = _currentSliderValue - _targetValue
-		if (-10 < difference && difference < 10) {
-			_showSuccessAlert()
+		if (-_maxDifferenceFromTarget < difference && difference < _maxDifferenceFromTarget) {
+			_showSuccessAlert(difference: difference)
+			_score += _maxDifferenceFromTarget - abs(difference)
 		} else {
-			_showFailAlert()
+			_showFailAlert(difference: difference)
 		}
-		_targetValue = 1 + Int(arc4random_uniform(100))
+		_setNewRound()
+		_updateLabels()
 	}
 	
 	@IBAction func sliderMoved(_ slider: UISlider) {
